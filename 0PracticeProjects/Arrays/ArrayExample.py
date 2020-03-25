5807ec8baaba5282625a79ef9699c51a43f889f5
Jesse Felix <jbfelix01@gmail.com>
2020-03-25 14:15:27 -0700

    Array
    Added some more things that's possible to do for arrays in "ArrayExample" also created "ArrayEmpty" folder for later use involving Arrays

--- 0PracticeProjects/Arrays/ArrayExample.py
+++ 0PracticeProjects/Arrays/ArrayExample.py
@@ -1,7 +1,20 @@
 #printing a string of numbers
 array = [1,2,3,4,5,6,7]
+
 array.insert(3,19)
 array.insert(7,8)
+
 array.remove(2)
 array.remove(5)
-print(array)
+
+array.append(9)
+array.append(10)
+
+array.pop()
+array.pop()
+
+print("The array is %s" %(array))
+
+length = len(array)
+
+print("There are %s characters in the array." % (length))
